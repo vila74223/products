217cf8c18702fb0c16768b3af9e5888eb8093b6e
vila74223 <vilahe74223@gmail.com>
2020-09-08 11:20:50 +0900

    store product name and price

---
+++ products.py
@@ -0,0 +1,15 @@
+#compare to for loop, while loop suits for the situation 
+#when not know how much times exactly the code need to run
+products = []
+while True:
+	name = input("Please enter the product name: ")
+	if name == q:
+		break
+	price = input("Please enter the product price: ")
+	products.append([name, price])
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	#p = [name, price]
+	#products.append(p)
+print(products)
